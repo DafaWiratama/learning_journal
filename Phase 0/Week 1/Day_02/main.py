deea252f52f881c27356028f761ecb767f4803a6
dafaw <dafawiratama13@gmail.com>
2022-02-15 13:10:55 +0700

    adding some comment for better readability

--- Phase 0/Week 1/Day_02/main.py
+++ Phase 0/Week 1/Day_02/main.py
@@ -1,3 +1,14 @@
+# Given Current list
+# [951, 402, 984, 651, 360, 69, 408, 319, 601, 485,
+# 980, 507, 725, 547, 544, 615, 83, 165, 141, 501,
+# 263, 617, 865, 575, 219, 390, 984, 592, 236, 105,
+# 942, 941, 386, 462, 47, 418, 907, 344, 236, 375,
+# 823, 566, 597, 978, 328, 615, 953, 345, 399, 162,
+# 758, 219, 918, 237, 412, 566, 826, 248, 866, 950,
+# 626, 949]
+# 1. Print all even numbers in the list before 918 with the same order as above
+# 2. Print "Done" when the list is printed
+
 numbers = [
     951, 402, 984, 651, 360, 69, 408, 319, 601, 485,
     980, 507, 725, 547, 544, 615, 83, 165, 141, 501,
@@ -10,6 +21,8 @@ numbers = [
 BREAKER = 918
 
 for number in numbers:
-    if number % 2 == 0: print(number)
-    if number == BREAKER: break
+    if number % 2 == 0:
+        print(number)
+    if number == BREAKER:
+        break
 print("Done")
