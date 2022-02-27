d96e4ad195e6ea96acc0722f29b25aec67e1c009
dafaw <dafawiratama13@gmail.com>
2022-02-27 22:05:16 +0700

    Update Week 2 Day Graded Challenge

--- Phase 0/Week 1/Day_03/notebook.py
+++ Phase 0/Week 1/Day_03/notebook.py
@@ -1,49 +1,59 @@
-# def test(list: list) -> int:
-#     # this function should take in a list of numbers and return the sum of all the numbers in the list
-#     toReturn = 0
-#     for e in list:
-#         toReturn += e
-#     return toReturn
+# # def test(list: list) -> int:
+# #     # this function should take in a list of numbers and return the sum of all the numbers in the list
+# #     toReturn = 0
+# #     for e in list:
+# #         toReturn += e
+# #     return toReturn
+# #
+# #
+# # def foo_a(a):
+# #     pass
+# # foo_a("")
+# #
+# #
+# # def foo_b(a: int):
+# #     pass
+# # foo_b(1)
+# #
+# #
+# # def foo_c(a=1):
+# #     pass
+# # foo_c()
+# #
+# #
+# # def foo_d(a=1):
+# #     return a
+# # b = foo_d()
+# #
+# #
+# # def foo_d(a=1) -> int:
+# #     return a
+# # b = foo_d()
 #
+# # c = lambda a, b: a + b
+# # import numpy as np
+# #
+# # tinggi = [1, 2, 3, 4, 5]
+# # berat = [65.5, 65.5, 65.5, 65.5, 65.5]
 #
-# def foo_a(a):
-#     pass
-# foo_a("")
-#
-#
-# def foo_b(a: int):
-#     pass
-# foo_b(1)
-#
-#
-# def foo_c(a=1):
-#     pass
-# foo_c()
-#
-#
-# def foo_d(a=1):
-#     return a
-# b = foo_d()
-#
-#
-# def foo_d(a=1) -> int:
-#     return a
-# b = foo_d()
-
-# c = lambda a, b: a + b
 # import numpy as np
 #
-# tinggi = [1, 2, 3, 4, 5]
-# berat = [65.5, 65.5, 65.5, 65.5, 65.5]
-
-import numpy as np
-
-
-def test():
-    pass
-
-a = np.arange(12)
-print(a.reshape(3, 2, -1))
+#
+# def test():
+#     pass
+#
+# a = np.arange(12)
+# print(a.reshape(3, 2, -1))
+#
+#
+#
 
 
-
+if __name__ == '__main__':
+    a = 0
+    while a < 5:
+        b = 0
+        while b < 5:
+            print(f"a={a}", f"b={b}")
+            b += 1
+        a += 1
